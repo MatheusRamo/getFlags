c60d2eaa54d83fb24abe50d0ee26eccaaf4e9ead
matheus <geof.matheus@gmail.com>
2022-01-25 03:42:53 -0300

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
-import react from "react";
-import Link from "next/link"
-import useSWR from "swr"
-
-async function getCountries() {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const data = await response.json()
-
-    const countries = data.map(country => {
-        const { name, population, flags } = country
-        return {
-            name: name.common,
-            population: population,
-            flag: flags.svg
-        }
-    })
-    console.log(countries)
-}
-
 
 const Home = ({data}) => {
     return (
         <div className="bg-gray-200">
             <ul className="p-2">
                 {data.map(country => (
-                    <li className="flex items-center p-2 m-1 border-solid border-2 border-sky-500 w-96">
+                    <li className="flex items-center justify-around p-2 m-1 text-center border-solid border-2 border-sky-500 w-96">
                         <h3 className="text-lg p-2 font-mono font-bold">{country.name}</h3>
                         <img className="border-solid border-2 border-sky-500 rounded-full w-12 h-12 p-2" src={country.flag}/>
                         <p className="p-2 font-medium text-lg">{country.population}</p>
